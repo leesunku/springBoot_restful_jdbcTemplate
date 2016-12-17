cb3b31a9265b8a915c5dbd7c00e8409bb0ed6bf5
k <tjsrn0524@naver.com>
2016-12-17 12:01:23 +0900

    2016/12/17

--- restfulApi/src/main/java/com/server/Application.java
+++ restfulApi/src/main/java/com/server/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-
+ 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
